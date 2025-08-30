b980cab5d5d9bd73d156c1186b6eebe0e444cd62
earthlingOnFire <78956999+earthlingOnFire@users.noreply.github.com>
2025-08-30 22:06:49 +0100

    First Commit

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoFleshPrison;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin {	
+  public const string PLUGIN_GUID = "com.earthlingOnFire.NoFleshPrison";
+  public const string PLUGIN_NAME = "NoFleshPrison";
+  public const string PLUGIN_VERSION = "1.0.0";
+  public static ManualLogSource logger;
+
+  private void Awake() {
+    gameObject.hideFlags = HideFlags.HideAndDontSave;
+    Plugin.logger = Logger;
+  }
+
+  private void Start() {
+    new Harmony(PLUGIN_GUID).PatchAll();
+    Plugin.logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+  }
+}
+
+[HarmonyPatch]
+public static class Patches {
+  [HarmonyPostfix]
+  [HarmonyPatch(typeof(EnemyIdentifier), "Start")]
+  public static void REMOVEFLESHPRISON(EnemyIdentifier __instance) {
+    if (__instance.enemyType == EnemyType.FleshPrison) {
+      AssistController.Instance.cheatsEnabled = true;
+      __instance.statue.eid = __instance;
+      __instance.statue.DeathEnd();
+    }
+  }
+}
---
+++ package/README.md
@@ -0,0 +1,9 @@
+# What does this mod do?
+- This mod will not allow the Flesh Prison to spawn under any circumstances. Not even with the spawner arm.
+- This mod silently enables cheats whenever it prevents the Flesh Prison from spawning.
+- Might break some custom levels, idk.
+
+# Manual Installation
+- Install BepInEx
+- Drag NoFleshPrison.dll to the BepInEx/plugins folder
+- Enjoy!
